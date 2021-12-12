e59f4df07c7e14de20584b82e715ca70b07756d4
slava0011 <mr.slawa2003@mail.ru>
2021-12-12 20:27:20 +0300

    added button

--- src/bsu/rfe/java/group7/lab3/Kondrashov/varB7/MainFrame.java
+++ src/bsu/rfe/java/group7/lab3/Kondrashov/varB7/MainFrame.java
@@ -202,5 +202,75 @@ public class MainFrame extends JFrame {
         // Установить область в верхнюю (северную) часть компоновки
         getContentPane().add(hboxRange, BorderLayout.SOUTH);
 
+        // Создать кнопку "Вычислить"
+        JButton buttonCalc = new JButton("Вычислить");
+        // Задать действие на нажатие "Вычислить" и привязать к кнопке
+        buttonCalc.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+                try {
+                    // Считать значения начала и конца отрезка, шага
+                    Double from =
+                            Double.parseDouble(textFieldFrom.getText());
+                    Double to =
+                            Double.parseDouble(textFieldTo.getText());
+                    Double step =
+                            Double.parseDouble(textFieldStep.getText());
+                    // На основе считанных данных создать новый экземпляр модели таблицы
+                    data = new GornerTableModel(from, to, step,
+                            MainFrame.this.coefficients);
+                    // Создать новый экземпляр таблицы
+                    JTable table = new JTable(data);
+                    // Установить в качестве визуализатора ячеек для класса Double разработанный визуализатор
+                    table.setDefaultRenderer(Double.class,
+                            renderer);
+                    // Установить размер строки таблицы в 30 пикселов
+                    table.setRowHeight(30);
+                    // Удалить все вложенные элементы из контейнера hBoxResult
+                    hBoxResult.removeAll();
+                    // Добавить в hBoxResult таблицу, "обѐрнутую" в панель с полосами прокрутки
+                    hBoxResult.add(new JScrollPane(table));
+                    // Обновить область содержания главного окна
+                    getContentPane().validate();
+                    // Пометить ряд элементов меню как доступных
+                    saveToTextMenuItem.setEnabled(true);
+                    saveToGraphicsMenuItem.setEnabled(true);
+                    searchValueMenuItem.setEnabled(true);
+
+                    for(int i = 0; i < data.getRowCount(); i++) {
+                        // Установить введенное значение в качестве знака
+                        renderer.setSignX(data.getSignX(i));
+                        // Обновить таблицу
+                        getContentPane().repaint();
+                    }
+                } catch (NumberFormatException ex) {
+                    // В случае ошибки преобразования чисел показать сообщение об ошибке
+                    JOptionPane.showMessageDialog(MainFrame.this,
+                            "Ошибка в формате записи числа с плавающей точкой", "Ошибочный формат числа",
+                            JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        });
+        // Создать кнопку "Очистить поля"
+        JButton buttonReset = new JButton("Очистить поля");
+        // Задать действие на нажатие "Очистить поля" и привязать к кнопке
+        buttonReset.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+                // Установить в полях ввода значения по умолчанию
+                textFieldFrom.setText("0.0");
+                textFieldTo.setText("1.0");
+                textFieldStep.setText("0.1");
+                // Удалить все вложенные элементы контейнера hBoxResult
+                hBoxResult.removeAll();
+                // Добавить в контейнер пустую панель
+                hBoxResult.add(new JPanel());
+                // Пометить элементы меню как недоступные
+                saveToTextMenuItem.setEnabled(false);
+                saveToGraphicsMenuItem.setEnabled(false);
+                searchValueMenuItem.setEnabled(false);
+                // Обновить область содержания главного окна
+                getContentPane().validate();
+            }
+        });
+
     }
 }
